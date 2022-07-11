33514f0d05e1d29ae3f00d5f6e9bcbf492403067
Stasja1 <73313921+Stasja1@users.noreply.github.com>
2022-07-11 18:22:16 +0300

    Update MainTest.java

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test;
 public class MainTest {
     @Test
     public void TaxEarningsTest() {
-        int earning = 1000, spending = 200, expectedResult = 60;
+        int earning = 1000;
+        int spending = 200;
+        int expectedResult = 60;
         int result = Main.taxEarnings(1000);
         Assertions.assertEquals(expectedResult, result);
     }
     @Test
     public void taxEarningsMinusSpendingsTest(){
-        int earning = 1000, spending = 200, expectedResult = 120 ;
+        int earning = 1000;
+        int spending = 200;
+        int expectedResult = 60;
         int result = Main.taxEarningsMinusSpendings(1000,200);
         Assertions.assertEquals(expectedResult, result);
     }
